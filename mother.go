977a00f014a60b04368324c191c4c917e16d39f2
Dakota Brink <dbrink@dealerinspire.com>
2018-05-31 20:49:37 -0400

    a json response

--- mother.go
+++ mother.go
@@ -1,14 +1,47 @@
 package main
 
+import ( // postgres..
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
 import (
 	"fmt"
 	"gopkg.in/gographics/imagick.v3/imagick"
+	_ "github.com/davecgh/go-spew/spew"
 	"io"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 )
+
+type Message struct {
+	Id    int64
+	Sid   string
+	Phone string
+	Body  string
+	Url   string
+}
+
+var db *sql.DB
+
+func createUser(phone string) {
+	sqlStatement := `INSERT INTO users (phone) VALUES ($1) ON CONFLICT DO NOTHING`
+	log.Printf("Creating user %s...", phone)
+	_, err := db.Exec(sqlStatement, phone)
+	if err != nil {
+		panic(err)
+	}
+}
+func createMessage(phone string, sid string, body string, url string) {
+	sqlStatement := `INSERT INTO messages (phone, sid, body, url) VALUES ($1, $2, $3, $4)`
+	_, err := db.Exec(sqlStatement, phone, sid, body, url)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // POST
 func handleSms(w http.ResponseWriter, r *http.Request) {
@@ -20,10 +53,14 @@ func handleSms(w http.ResponseWriter, r *http.Request) {
 	}
 
 	smsSid      := form.Get("SmsSid")
-	from        := form.Get("From")[1:]
+	phone       := form.Get("From")[1:]
+	//createUser(phone)
 	numMedia, _ := strconv.Atoi(form.Get("NumMedia"))
 	for i:= 0; i <= numMedia - 1; i++ {
-		cacheImage(from, smsSid, form.Get(fmt.Sprintf("MediaUrl%d", i)))
+		url := form.Get(fmt.Sprintf("MediaUrl%d", i))
+		cacheImage(phone, smsSid, url)
+		log.Println("Creating message...")
+		createMessage(phone, smsSid, form.Get("Body"), url)
 	}
 }
 
@@ -50,24 +87,67 @@ func cacheImage(phone string, sid string, url string) error {
 }
 
 // GET
-func provideImg(w http.ResponseWriter, r *http.Request) {
+func provideImages(w http.ResponseWriter, r *http.Request) {}
+
+// GET
+func provideImage(w http.ResponseWriter, r *http.Request) {
 	imagick.Initialize()
 	defer imagick.Terminate()
 
 	mw := imagick.NewMagickWand()
 	defer mw.Destroy()
 
-	query  := r.URL.Query()
-	phone  := query.Get("phone")
-	sid    := query.Get("sid")
-	width  := query.Get("width")
-	height := query.Get("height")
+	//query  := r.URL.Query()
+	//phone  := query.Get("phone")
+	//sid    := query.Get("sid")
+	//width  := query.Get("width")
+	//height := query.Get("height")
+}
 
+// GET
+func provideMessages(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query(`SELECT * FROM messages WHERE phone = $1`, r.URL.Query().Get("phone"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
 
+	var (
+		sid   string
+		phone string
+		body  string
+		url   string
+		id    int64
+	)
+	var messages []Message
+	for rows.Next() {
+		err := rows.Scan(&sid, &phone, &body, &url, &id)
+		if err != nil {
+			panic(err)
+		}
+		messages = append(messages, Message{Sid: sid, Phone: phone, Body: body, Url: url, Id: id})
+	}
+
+	jData, err := json.Marshal(messages)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jData)
 }
 
 func main() {
+	connStr := "user=postgres dbname=mother sslmode=disable"
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	http.HandleFunc("/sms", handleSms)
-	http.HandleFunc("/img", provideImg)
+	http.HandleFunc("/image", provideImage)
+	http.HandleFunc("/images", provideImages)
+	http.HandleFunc("/messages", provideMessages)
 	http.ListenAndServe(":8080", nil)
 }
